1fe7af0a0e8c639036ec57b84a0f710512cfb0fb
Evan C <evancummings3@gmail.com>
2023-02-18 01:43:16 -0600

    last commit

--- src/Operationbutton.js
+++ src/Operationbutton.js
@@ -1,6 +1,6 @@
 import { ACTIONS } from "./App"
 
-export default function Operationbutton({ dispatch, operation }) {
+export default function OperationButton({ dispatch, operation }) {
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
